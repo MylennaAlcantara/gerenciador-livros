ae420f5c6a848088c358f4ea3f0b32e55a37f36b
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2024-03-05 12:31:38 -0300

    feat: novos testes para tela de ListaLivro, excluir livro, renderizar e funcionamento do modal

--- frontend/src/pages/tests/listaLivro.spec.tsx
+++ frontend/src/pages/tests/listaLivro.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import { screen } from '@testing-library/react'
 import { BrowserRouter } from "react-router-dom";
 import { ListaLivros } from "../listaLivros";
@@ -12,22 +12,55 @@ const renderComponent = () => {
 }
 
 describe("ListaLivro", () => {
-    it("Shold be render book list", () => {
+    it("Should be render book list", () => {
         renderComponent();
     });
 
-    it("Shold exclude book correctely", ()=> {
-        const monkLivros = {id: 1, titulo: "livro 1"};
-
+    it('Should call book API and display the data correctely', async () => {
+        const mockLivros = [{ id: 1, titulo: 'Livro 1' }, { id: 2, titulo: 'Livro 2' }];
+        
         global.fetch = jest.fn().mockResolvedValue({
-            json: jest.fn().mockResolvedValue(monkLivros)
-        })
-
-        renderComponent();
-
-        fireEvent.click(screen.getByText('Excluir'));
-
+            json: jest.fn().mockResolvedValue(mockLivros),
+        });
     
-        expect(screen)
-    })
+        renderComponent();
+    
+        await waitFor(() => {
+            expect(screen.getByText('Livro 1')).toBeInTheDocument();
+            expect(screen.getByText('Livro 2')).toBeInTheDocument();
+        });
+    });
+    
+    it('Should open and close the view  modal correctely', async () => {
+        renderComponent();
+    
+        fireEvent.click(screen.getByText('Visualizar'));
+    
+        expect(screen.getByTestId('modal')).toBeInTheDocument();
+    
+        fireEvent.click(screen.getByTestId('fechar-modal'));
+    
+        await waitForElementToBeRemoved(() => screen.getByTestId('modal'));
+    });
+    
+    it('Should exclude book correctely', async () => {
+        const mockLivros = [{ id: 1, titulo: 'Livro 1' }];
+        
+        global.fetch = jest.fn().mockResolvedValue({
+            json: jest.fn().mockResolvedValue(mockLivros),
+        });
+    
+        renderComponent();
+    
+        await waitFor(() => {
+            expect(screen.getByText('Livro 1')).toBeInTheDocument();
+        });
+    
+        fireEvent.click(screen.getByText('Excluir'));
+    
+        await waitForElementToBeRemoved(() => screen.getAllByText('Livro 1'));
+    
+        expect(screen.getByText('Ainda não existem livros cadastrados!')).toBeInTheDocument();
+    });
+    
 })
